f04b960d5eb3b52e6ceffcd889fb1b2ca95aa8c9
Jannis Jorre <jannis@jorre.dev>
2020-09-08 13:33:38 +0200

    install haskell-language-server and pin dependencies

--- shell.nix
+++ shell.nix
@@ -1,15 +1,32 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
-  inherit (nixpkgs) pkgs;
-  inherit (pkgs) haskellPackages;
+  pinnedPkgs =
+    nixpkgs.pkgs.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixpkgs-channels";
+
+      # change this to update ghc and all other dependencies (also see below for language-server)
+      rev = "a31736120c5de6e632f5a0ba1ed34e53fc1c1b00";
+      sha256 = "0xfjizw6w84w1fj47hxzw2vwgjlszzmsjb8k8cgqhb379vmkxjfl";
+    };
 
   project = import ./release.nix;
+
+  pinned = import pinnedPkgs {
+    config = nixpkgs.config;
+  };
 in
-pkgs.stdenv.mkDerivation {
+pinnedPkgs.stdenv.mkDerivation {
   name = "shell";
   buildInputs = project.env.nativeBuildInputs ++ [
-    haskellPackages.cabal-install
-    haskellPackages.ghcid
-    pkgs.entr
+    pinned.haskellPackages.cabal-install
+
+    pinned.haskellPackages.ghcid
+    # make sure the ghc version referenced here is the same as installed above
+    pinned.haskell.packages.ghc884.haskell-language-server
+
+    pinned.haskellPackages.hlint
+
+    pinned.entr
   ];
 }
